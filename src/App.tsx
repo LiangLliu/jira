11a914190835d6f5e93ba7b239de8a32c6d7054b
LiangLliu <max1141847753@gmail.com>
2021-12-03 19:47:59 +0800

    feat: 添加ErrorBoundary

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,18 @@ import "./App.css";
 import { useAuth } from "./context/auth-context";
 import { AuthenticatedApp } from "./authenticated-app";
 import { UnauthenticatedApp } from "./unauthenticated-app";
+import { ErrorBoundary } from "./components/error-boundary";
+import { FullPageErrorFallback } from "./components/lib";
 
 function App() {
   const { user } = useAuth();
 
   return (
     <div className="App">
-      {user ? <AuthenticatedApp /> : <UnauthenticatedApp />}
+      {/*全局错误处理*/}
+      <ErrorBoundary failBackRender={FullPageErrorFallback}>
+        {user ? <AuthenticatedApp /> : <UnauthenticatedApp />}
+      </ErrorBoundary>
     </div>
   );
 }
