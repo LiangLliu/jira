e5ffbc29445ebbfbf90a13aa3992e4ed5857e0ec
LiangLliu <max1141847753@gmail.com>
2021-12-03 20:26:11 +0800

    feat: 使用useRef hock 实现useDocumentTitle

--- src/screans/project-list/index.tsx
+++ src/screans/project-list/index.tsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import { List } from "./list";
 import { SearchPanel } from "./search-panel";
-import { useDebounce } from "utils";
+import { useDebounce, useDocumentTitle } from "utils";
 import styled from "@emotion/styled";
 import { Typography } from "antd";
 import { useProjects } from "../../utils/Project";
@@ -15,8 +15,9 @@ export const ProjectListScreen = () => {
 
   const debouncedParam = useDebounce(param, 200);
   const { isLoading, error, data: list } = useProjects(debouncedParam);
-
   const { data: users } = useUsers();
+
+  useDocumentTitle("项目列表");
 
   return (
     <Container>
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 
 export const isFalsy = (value: unknown) => (value === 0 ? true : !value);
 export const isVoid = (value: unknown) =>
@@ -37,4 +37,36 @@ export const useDebounce = <T>(value: T, delay?: number): T => {
   }, [value, delay]);
 
   return debounceValue;
+};
+
+export const useArray = <T>(initialArray: T[]) => {
+  const [value, setValue] = useState(initialArray);
+  return {
+    value,
+    setValue,
+    add: (item: T) => setValue([...value, item]),
+    clear: () => setValue([]),
+    removeIndex: (index: number) => {
+      const copy = [...value];
+      copy.splice(index, 1);
+      setValue(copy);
+    },
+  };
+};
+
+export const useDocumentTitle = (title: string, keepOnUnmount = true) => {
+  // useRef保持值不变
+  const oldTitle = useRef(document.title).current;
+
+  useEffect(() => {
+    document.title = title;
+  }, [title]);
+
+  useEffect(() => {
+    return () => {
+      if (!keepOnUnmount) {
+        document.title = oldTitle;
+      }
+    };
+  }, [keepOnUnmount, oldTitle]);
 };
