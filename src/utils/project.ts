013bbee22478a5b3f474058068f6425826a39246
LiangLliu <max1141847753@gmail.com>
2021-12-25 22:58:51 +0800

    chore: 用useCallback解决无限渲染问题

--- src/utils/project.ts
+++ src/utils/project.ts
@@ -1,6 +1,6 @@
 import { useAsync } from "utils/use-async";
 
-import { useEffect } from "react";
+import { useCallback, useEffect } from "react";
 
 import { useHttp } from "utils/http";
 import { Project } from "../screans/project-list/list";
@@ -10,15 +10,16 @@ export const useProjects = (param?: Partial<Project>) => {
   const client = useHttp();
   const { run, ...result } = useAsync<Project[]>();
 
-  const fetchProjects = () =>
-    client("projects", { data: cleanObject(param || {}) });
+  const fetchProjects = useCallback(
+    () => client("projects", { data: cleanObject(param || {}) }),
+    [client, param]
+  );
 
   useEffect(() => {
     run(fetchProjects(), {
       retry: fetchProjects,
     });
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [param]);
+  }, [fetchProjects, param, run]);
 
   return result;
 };
